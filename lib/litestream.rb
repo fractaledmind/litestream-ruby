717e819588fe20c97830931f9b4090bf0cd04a94
Stephen Margheim <stephen.margheim@gmail.com>
2023-12-11 22:10:48 +0100

    Add configuration for the gem

--- lib/litestream.rb
+++ lib/litestream.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 module Litestream
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :database_path, :replica_url, :replica_key_id, :replica_access_key
+
+    def initialize
+      @mailer_sender = "donotreply@example.com"
+    end
+  end
 end
 
 require_relative "litestream/version"
